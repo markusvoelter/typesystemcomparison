9b2086bdc65e4250c132fd39bcfd8de51a552881
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-03-21 14:07:21 +0100

    shown other parts of the java validator

--- tsarticle/code/GuiDslJavaValidator.java
+++ tsarticle/code/GuiDslJavaValidator.java
@@ -1,5 +1,9 @@
 public class GuiDslJavaValidator extends AbstractGuiDslJavaValidator {
 
+	public final static String INCOMPATIBLE_TYPES = "incompatible_types";
+	@Inject private GuiDslTypeProvider guiDslTypeProvider;
+	@Inject private TypeConformance conformance;
+
 	@Check
 	public void check(Expression expr) {
 		checkType(expr);
@@ -11,13 +15,34 @@ public class GuiDslJavaValidator extends AbstractGuiDslJavaValidator {
 	 */
 	@Check
 	public void check(TextWidget widget) {
-		Attribute attr = widget.getAttr();
-		Type type = guiDslTypeProvider.getType(attr);
+		Type type = guiDslTypeProvider.getType(widget.getAttr());
 		if (type.eClass() == GuiDslPackage.Literals.BOOLEAN_TYPE) {
 			error("A text widget may not refer to a boolean attribute.",
 					GuiDslPackage.Literals.WIDGET__ATTR, INCOMPATIBLE_TYPES);
 		}
 	}
+
+	/**
+	 * 3) Checkbox widgets may only refer to boolean attributes.
+	 */
+	@Check
+	void check(CheckBoxWidget widget) {
+		Type type = guiDslTypeProvider.getType(widget.getAttr());
+		if (type.eClass() != GuiDslPackage.Literals.BOOLEAN_TYPE) {
+			error("Checkbox widget may only refer to boolean attributes.",
+					GuiDslPackage.Literals.WIDGET__ATTR, INCOMPATIBLE_TYPES);
+		}
+	}
+	
+	@Check
+	void checkWidget(Widget widget) {
+		checkType(widget, GuiDslPackage.Literals.WIDGET__ATTR);
+	}
+	
+	protected void checkType(EObject object) {
+		checkType(object, null);
+	}
+	
 	/**
 	 * @param object - EObject from the DSL to check type, e.g. Widget
 	 * @param feature - feature for error message location, e.g. Attribute reference,
@@ -26,6 +51,10 @@ public class GuiDslJavaValidator extends AbstractGuiDslJavaValidator {
 	protected void checkType(EObject object, EStructuralFeature feature) {
 		Type expectedType = guiDslTypeProvider.getExpectedType(object);
 		Type actualType = guiDslTypeProvider.getType(object);
+		if (actualType instanceof CyclicDependencyType) {
+			error("Type is part of a cyclic dependency.", feature,
+					INCOMPATIBLE_TYPES); return;
+		}
 		if (expectedType == null)
 			return;
 		if (!conformance.isAssignable(expectedType, actualType)) {
@@ -34,3 +63,5 @@ public class GuiDslJavaValidator extends AbstractGuiDslJavaValidator {
 					INCOMPATIBLE_TYPES);
 		}
 	}
+	
+}
